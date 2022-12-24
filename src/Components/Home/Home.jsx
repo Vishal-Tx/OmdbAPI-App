cd028553dbb3f7c1257be91289e91ec6e9620c07
vishal-Tx <vishalguptaa2z111@gmail.com>
2022-12-24 17:41:21 +0530

    searchtext css done.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,75 +1,4 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
 import InfiniteScroll from "react-infinite-scroll-component";
-
-// const OmdbContainer = () => {
-//   const [movies, setMovies] = useState([]);
-//   const [searchTerm, setSearchTerm] = useState("avenger");
-//   const [page, setPage] = useState(1);
-//   const [hasMore, setHasMore] = useState(true);
-//   const [isLoading, setIsLoading] = useState(false);
-//   console.log("Movies", movies);
-
-//   useEffect(() => {
-//     setIsLoading(true);
-//     axios
-//       .get(
-//         `http://www.omdbapi.com/?apikey=667984e7&s=${searchTerm}&page=${page}`
-//       )
-//       .then((response) => {
-//         console.log(response);
-//         setMovies((prevMovies) => [...prevMovies, ...response?.data?.Search]);
-//         setHasMore(response?.data?.Search?.length > 0);
-//         setIsLoading(false);
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//         setIsLoading(false);
-//       });
-//   }, [searchTerm, page]);
-
-//   const handleSearch = (event) => {
-//     setSearchTerm(event.target.value);
-//     setPage(1);
-//     setMovies([]);
-//   };
-
-//   const handleLoadMore = () => {
-//     setPage((prevPage) => prevPage + 1);
-//   };
-
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         value={searchTerm}
-//         onChange={handleSearch}
-//         placeholder="Search for a movie..."
-//       />
-//       <InfiniteScroll
-//         dataLength={movies?.length}
-//         next={handleLoadMore}
-//         hasMore={hasMore}
-//         loader={<h4>Loading...</h4>}
-//         endMessage={
-//           <p style={{ textAlign: "center" }}>
-//             <b>Yay! You have seen it all</b>
-//           </p>
-//         }
-//       >
-//         {movies.map((movie) => (
-//           <div key={movie.imdbID}>
-//             <h3>{movie.Title}</h3>
-//             <p>Year: {movie.Year}</p>
-//             <img src={movie.Poster} alt={movie.Title} />
-//           </div>
-//         ))}
-//       </InfiniteScroll>
-//     </div>
-//   );
-// };
-
-// export default OmdbContainer;
 
 import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
@@ -77,15 +6,16 @@ import data from "../../assets/data.json";
 import MovieCard from "../Card/MovieCard";
 import { Box, CircularProgress, Typography } from "@mui/material";
 import Navbar from "../Navbar/Navbar";
+import "./style.css";
 
 const OmdbContainer = () => {
   const [movies, setMovies] = useState([]);
 
-  const [localSearchTerm, setLocalSearchTerm] = useState("");
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [localSearchTerm, setLocalSearchTerm] = useState("");
 
   //   console.log("data", data);
 
@@ -95,40 +25,37 @@ const OmdbContainer = () => {
   );
 
   const randomNumber = useMemo(() => {
-    return Math.floor(Math.random() * 4);
+    return Math.floor(Math.random() * 5);
   }, []);
 
-  const dict = ["Love", "war", "red", "heart"];
+  const dict = ["Love", "war", "red", "heart", "Black"];
 
   //   console.log("currYear", currYear);
   //   console.log("randomNumber", randomNumber);
   let sTerm = dict[randomNumber];
   useEffect(() => {
     setIsLoading(true);
-    axios
-      .get(
-        `http://www.omddbapi.com/?apikey=667984e7&y=${currYear}&s="${sTerm}"&type="movie"&page=${page}`
-      )
-      .then((response) => {
-        console.log("response", response);
-        if (response.data?.Response === "False") return setHasMore(false);
-        else setHasMore(true);
-        setMovies((prevMovies) => [...prevMovies, ...response?.data?.Search]);
+    // axios
+    //   .get(
+    //     `http://www.omddbapi.com/?apikey=667984e7&y=${currYear}&s="${sTerm}"&type="movie"&page=${page}`
+    //   )
+    //   .then((response) => {
+    //     console.log("response", response);
+    //     if (response.data?.Response === "False") return setHasMore(false);
+    //     else setHasMore(true);
+    //     setMovies((prevMovies) => [...prevMovies, ...response?.data?.Search]);
 
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error(error);
-        setIsLoading(false);
-        setError("Something went wrong. Please try again!");
-      });
+    //     setIsLoading(false);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //     setIsLoading(false);
+    //     setError("Something went wrong. Please try again!");
+    //   });
     // setMovies((prevMovies) => [...prevMovies, ...data]);
-    // setMovies((prevMovies) => [...data]);
+    setMovies((prevMovies) => [...data]);
+    setHasMore(false);
   }, [page]);
-
-  const handleLocalSearch = (event) => {
-    setLocalSearchTerm(event.target.value);
-  };
 
   const handleLoadMore = () => {
     setPage((prevPage) => prevPage + 1);
@@ -139,16 +66,22 @@ const OmdbContainer = () => {
     return regex.test(movie.Title);
   });
   console.log("hasMore", hasMore);
+
+  const handleLocalSearch = (event) => {
+    setLocalSearchTerm(event.target.value);
+  };
   return (
     <>
-      <Navbar setPage={setPage} setMovies={setMovies} page={page} />
+      <Navbar
+        localSearchTerm={localSearchTerm}
+        handleLocalSearch={handleLocalSearch}
+      />
       {error ? (
         <Box sx={{ textAlign: "center" }}>
           <Typography>{error}</Typography>
         </Box>
       ) : (
         <div>
-          <Typography>Searching for {sTerm}</Typography>
           <div className="search-container">
             {/* <input
             type="text"
@@ -156,12 +89,12 @@ const OmdbContainer = () => {
             onChange={handleApiSearch}
             placeholder="Search OMDb API..."
           /> */}
-            <input
+            {/* <input
               type="text"
               value={localSearchTerm}
               onChange={handleLocalSearch}
               placeholder="Search among fetched results..."
-            />
+            /> */}
           </div>
           <InfiniteScroll
             dataLength={filteredMovies.length}
@@ -183,6 +116,19 @@ const OmdbContainer = () => {
               </p>
             }
           >
+            <Box
+              sx={{
+                ml: { lg: "50px" },
+                mt: "10px",
+                display: "flex",
+                justifyContent: { lg: "flex-start", xs: "center" },
+              }}
+            >
+              <Typography className="searchTitle" variant="h4">
+                Searching for "<span style={{ color: "white" }}>{sTerm}</span>"
+                <span style={{ color: "red" }}>*</span>
+              </Typography>
+            </Box>
             <Box
               sx={{
                 display: "flex",
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,34 +2,38 @@ import { Box, Link, Typography } from "@mui/material";
 import axios from "axios";
 import React, { useState } from "react";
 
-const Navbar = ({ setPage, setMovies, page }) => {
+const Navbar = ({ handleLocalSearch, localSearchTerm }) => {
   const [apiSearchTerm, setApiSearchTerm] = useState("");
 
-  const handleApiSearch = (event) => {
-    setApiSearchTerm(event.target.value);
-    setPage(1);
-    setMovies([]);
-    console.log("apiSearchTerm", apiSearchTerm);
-    axios
-      .get(
-        `http://www.omdbapi.com/?apikey=667984e7&s=${apiSearchTerm}&page=${page}`
-      )
-      .then((response) => {
-        console.log("odresponse", response);
-        setMovies((prevMovies) => [...prevMovies, ...response.data.Search]);
-        // setHasMore(response.data.Search.length > 0);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+  // const handleApiSearch = (event) => {
+  //   setApiSearchTerm(event.target.value);
+  //   setPage(1);
+  //   setMovies([]);
+  //   console.log("apiSearchTerm", apiSearchTerm);
+  //   axios
+  //     .get(
+  //       `http://www.omdbapi.com/?apikey=667984e7&s=${apiSearchTerm}&page=${page}`
+  //     )
+  //     .then((response) => {
+  //       console.log("odresponse", response);
+  //       setMovies((prevMovies) => [...prevMovies, ...response.data.Search]);
+  //       // setHasMore(response.data.Search.length > 0);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // };
+
   return (
     <Box
       sx={{
         display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "flex-start",
         backgroundColor: "#3C2A21",
         color: "white",
         py: 2,
+        px: 6,
       }}
     >
       <a href="/">
@@ -37,9 +41,9 @@ const Navbar = ({ setPage, setMovies, page }) => {
       </a>
       <input
         type="text"
-        value={apiSearchTerm}
-        onChange={handleApiSearch}
-        placeholder="Search OMDb API..."
+        value={localSearchTerm}
+        onChange={handleLocalSearch}
+        placeholder="Search among fetched results..."
       />
     </Box>
   );
