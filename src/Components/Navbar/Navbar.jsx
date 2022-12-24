830e562b5b2b1a3b0147bf8f5387eda1c12e4fe9
vishal-Tx <vishalguptaa2z111@gmail.com>
2022-12-24 23:14:47 +0530

    api search implemented

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,55 @@
 import { Box, Link, Typography } from "@mui/material";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const Navbar = ({ handleLocalSearch, localSearchTerm }) => {
-  const [apiSearchTerm, setApiSearchTerm] = useState("");
+const Navbar = ({
+  handleLocalSearch,
+  localSearchTerm,
+  setMovies,
+  setError,
+  setApiSearchTerm,
+  apiSearchTerm,
+}) => {
+  const [page, setPage] = useState(1);
+  const [debouncer, setDebouncer] = useState("");
 
-  // const handleApiSearch = (event) => {
-  //   setApiSearchTerm(event.target.value);
-  //   setPage(1);
-  //   setMovies([]);
-  //   console.log("apiSearchTerm", apiSearchTerm);
-  //   axios
-  //     .get(
-  //       `http://www.omdbapi.com/?apikey=667984e7&s=${apiSearchTerm}&page=${page}`
-  //     )
-  //     .then((response) => {
-  //       console.log("odresponse", response);
-  //       setMovies((prevMovies) => [...prevMovies, ...response.data.Search]);
-  //       // setHasMore(response.data.Search.length > 0);
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // };
+  const key = import.meta.env.VITE_APP_APIKEY;
+
+  useEffect(() => {
+    let delay = setTimeout(() => {
+      if (apiSearchTerm.length > 0) setDebouncer(apiSearchTerm);
+    }, 400);
+
+    return () => clearTimeout(delay);
+    if (apiSearchTerm.length > 0) handleApiSearch();
+  }, [apiSearchTerm]);
+
+  useEffect(() =>)
+
+  const handleChange = (event) => {
+    setApiSearchTerm(event.target.value);
+  };
+  const handleApiSearch = async (event) => {
+    setPage(1);
+    setMovies([]);
+    console.log("apiSearchTerm", apiSearchTerm);
+    try {
+      const response = await axios.get(
+        `http://www.omdbapi.com/?apikey=${key}&s=${apiSearchTerm}&page=${page}`
+      );
+      console.log("response", response);
+      if (response?.data?.Response === "False")
+        throw new Error(response.data.Error);
+      setError(null);
+      setMovies((prevMovies) => [
+        ...(prevMovies || []),
+        ...(response?.data?.Search || []),
+      ]);
+    } catch (error) {
+      setError(error.message);
+      // console.log("error", error.message);
+    }
+  };
 
   return (
     <Box
@@ -36,9 +63,18 @@ const Navbar = ({ handleLocalSearch, localSearchTerm }) => {
         px: 6,
       }}
     >
-      <a href="/">
+      <a
+        href="/"
+        style={{ textDecoration: "none", color: "white", marginRight: "15px" }}
+      >
         <Typography variant="h4">Moviezzz</Typography>
       </a>
+      <input
+        type="text"
+        value={apiSearchTerm}
+        onChange={handleChange}
+        placeholder="Search OMDb API..."
+      />
       <input
         type="text"
         value={localSearchTerm}
